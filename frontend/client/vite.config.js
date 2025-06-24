43da22851614fbb14d45938cb3c7a7ed7bb5e8e1
nidhi namdev <nidhi.namdev@kalvium.community>
2025-06-24 20:24:18 +0530

    fix: added base path for Netlify deployment

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 👈 Fix for Netlify blank screen
   server: {
     proxy: {
-      '/api': 'http://localhost:5000', // or whatever port your backend runs on
+      '/api': 'http://localhost:5000',
     },
   },
 })
